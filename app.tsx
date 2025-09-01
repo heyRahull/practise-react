eb18749a5a1375b8e5d773af8cf8dad896928857
Rahul Agarwal <rahul123agarwal99@gmail.com>
2025-09-01 10:42:32 +0530

    Create a function that adds two numbers (strictly typed).

---
+++ app.tsx
@@ -0,0 +1,10 @@
+// const userName:string="Rahul";
+
+// console.log(userName)
+
+const sum = (num1:number, num2:number): number => {
+    const ans = num1 + num2;
+    return ans;
+}
+
+console.log(sum(20,20));
