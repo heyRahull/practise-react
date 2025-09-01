32c48e99244c3d146ebb9f6fc0e365ee53d5f797
Rahul Agarwal <rahul123agarwal99@gmail.com>
2025-09-01 10:47:41 +0530

    function to print name and age in TS

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Rahul");
+// const userName:string="Rahul";
+// console.log(userName)
+// 1. Create a function that adds two numbers (strictly typed).
+// const sum = (num1:number, num2:number): number => {
+//     const ans = num1 + num2;
+//     return ans;
+// }
+// console.log(sum(20,20));
+// 2. Write a function that takes a name (string) and age (number) and prints "Hello Rahul, you are 25".
+var printName = function (name, age) {
+    console.log("Hello ".concat(name, ", you are ").concat(age));
+};
+printName("Rahul", 25);
+printName("Ram", 26);
--- app.tsx
+++ app.tsx
@@ -2,9 +2,20 @@
 
 // console.log(userName)
 
-const sum = (num1:number, num2:number): number => {
-    const ans = num1 + num2;
-    return ans;
+// 1. Create a function that adds two numbers (strictly typed).
+
+// const sum = (num1:number, num2:number): number => {
+//     const ans = num1 + num2;
+//     return ans;
+// }
+
+// console.log(sum(20,20));
+
+// 2. Write a function that takes a name (string) and age (number) and prints "Hello Rahul, you are 25".
+
+const printName = (name:string, age:number): void => {
+    console.log(`Hello ${name}, you are ${age}`);
 }
 
-console.log(sum(20,20));
+printName("Rahul", 25);
+printName("Ram", 26);
